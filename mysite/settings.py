89d09e10d1ae7224fec466e8175b756c536ed7c1
HuseynHasan <huseynhasan80@gmail.com>
2022-12-17 13:41:43 +0400

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestinAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question']}),
+        ('Tarix cədvəli', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+
+admin.site.register(Question, QuestinAdmin)
+admin.site.register(Choice)
